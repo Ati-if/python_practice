6dba46dbb16456cb99f6e16fec67140c1340d9c6
Ati-if <natif3101@gmail.com>
2025-03-19 11:35:42 +0500

    practice of if else

--- sqrt.py
+++ sqrt.py
@@ -19,3 +19,13 @@ c = int(input("Enter the value of side 'C'"))
 d = math.sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
 
 print((f"The value of D is {round (d , 2)}"))
+
+
+age = int(input("Enter your age"))
+
+if age >= 18:
+  print ("You are an adult")
+elif age <= 18:
+  print("You are not an adult person")
+else:
+  print("Invalid input")
