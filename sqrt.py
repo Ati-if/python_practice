c1be4ed79a7044d99cf931aa7632a2a8bedf25ea
Ati-if <natif3101@gmail.com>
2025-03-19 11:18:09 +0500

    round off practice

--- sqrt.py
+++ sqrt.py
@@ -7,3 +7,15 @@ c = int(input("Enter the value of side 'C'"))
 d = math.sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
 
 print((f"The value of D is {d}"))
+
+
+
+import math
+
+a = int(input("Enter the value of side 'A'"))
+b = int(input("Enter the value of side 'B'"))
+c = int(input("Enter the value of side 'C'"))
+
+d = math.sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+
+print((f"The value of D is {round (d , 2)}"))
