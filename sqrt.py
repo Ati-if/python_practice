e29e8d697dc52ec446f60af4d8a9389cf40767be
Ati-if <natif3101@gmail.com>
2025-03-19 11:10:40 +0500

    practice of sqrt

---
+++ sqrt.py
@@ -0,0 +1,9 @@
+import math
+
+a = int(input("Enter the value of side 'A'"))
+b = int(input("Enter the value of side 'B'"))
+c = int(input("Enter the value of side 'C'"))
+
+d = math.sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+
+print((f"The value of D is {d}"))
