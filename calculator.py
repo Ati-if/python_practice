a5269efa3e80b6b7bb1dc6d8ca35fcd82c079cb2
Ati-if <natif3101@gmail.com>
2025-03-19 19:40:33 +0500

    calculator practice 2

--- calculator.py
+++ calculator.py
@@ -6,3 +6,24 @@ print(num1 + num2)
 print(num1 - num2)
 print(num1 * num2)
 print(num1 / num2)
+
+
+
+num1 = float(input("Enter the first number"))
+num2 = float(input("Enter the second number"))
+
+operator = input("Enter an operator (+  -  *  / )")
+
+
+if operator  == "+":
+  result = num1 + num2
+  print(result)
+elif operator == "-":
+  result = num1 - num2
+  print(result)
+elif operator == "*":
+  result = num1 * num2
+  print(result)
+elif operator == "/": 
+  result = num1 / num2
+  print(result)
