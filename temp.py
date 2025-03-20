0adcd0b13ed0660029d930d855dcc49d30b8ae15
Ati-if <natif3101@gmail.com>
2025-03-20 19:40:47 +0500

    test work

--- temp.py
+++ temp.py
@@ -2,10 +2,10 @@ unit = input("Is this temperature in celsius or fahrenheit")
 temp = float(input("Enter the temperature"))
 
 if unit == "celsius":
-  temp = (temp * 9/5) + 32
+  temp = round(temp * 9/5 ) + 32 ,2
   print("The temperature in fahrenheit is {temp}")
 elif unit == "fahrenheit":
-  temp = (temp - 32) * 5/9
+  temp = round(temp - 32) * 5/9 ,3
   print("The temperature in celsius is {temp}")
 else:
   print("Invalid unit")
