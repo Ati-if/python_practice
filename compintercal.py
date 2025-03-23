f73371b17e14ce660ff702ac94683e5044db547a
Ati-if <natif3101@gmail.com>
2025-03-23 15:30:48 +0500

    compound interest calculator

---
+++ compintercal.py
@@ -0,0 +1,13 @@
+principle = 5
+rate = 4
+time = 20
+
+
+while principle <= 5 :
+  principle = float(input("Enter the principle amount"))
+  if principle <= 0:  
+    print("Your principle can't be less or equal to 5")
+
+
+
+print(principle)
