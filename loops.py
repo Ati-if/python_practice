1000e3f6e6f5a9dd58a7d5afb7d2bbc7e64f4267
Ati-if <natif3101@gmail.com>
2025-03-23 14:50:13 +0500

    practice of while loop

--- loops.py
+++ loops.py
@@ -4,4 +4,14 @@ while name ==  "":
   print("You did not enter your full name")
   name = input("Enter your name")
 
-print(f"Hello {name}") 
+print(f"Hello {name}")
+
+
+
+food = input("Enter your favourite food (q to quit):")
+
+while not food ==  "q":
+  print(f"You like {food}")
+  food = input("Enter your favourite food (q to quit):")
+
+print("Good Bye")  
