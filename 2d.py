9e5af863a1462a299480626a0dbc07128111e269
Ati-if <natif3101@gmail.com>
2025-03-25 09:04:09 +0500

    2 Dimensional code

---
+++ 2d.py
@@ -0,0 +1,9 @@
+fruits =     ["apple", "orange", "banana", "coconut"]
+vegetables = ["potatoes", "tomatoes", "carrots", "cucumbers"]
+meat =       ["chicken", "beef", "pork", "fish"]
+
+grocery_list = [fruits, vegetables, meat]
+for x in grocery_list:
+  print(x)
+
+print(grocery_list[0][2])
