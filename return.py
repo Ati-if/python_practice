4453e774d25cc12a1a66982a149050c57eda14ea
Ati-if <natif3101@gmail.com>
2025-04-05 10:53:02 +0500

    return function

---
+++ return.py
@@ -0,0 +1,22 @@
+def add(x , y):
+  z = x + y
+  return z
+
+def subtract(x , y):
+  z = x - y
+  return z
+
+def multiply(x , y):
+  z = x * y
+  return z
+
+
+def divide(x , y):
+  z = x / y
+  return z
+
+
+print(add(3 , 4))
+print(subtract(5 , 3))
+print(multiply(3 , 4))
+print(divide(25 , 5)) 
