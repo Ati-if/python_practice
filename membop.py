78fc4e71e600a62d0da9437deeaaaefeadeb7dbe
Ati-if <natif3101@gmail.com>
2025-04-06 15:04:14 +0500

    member operator practice

--- membop.py
+++ membop.py
@@ -6,3 +6,23 @@ if letter in word:
    print(f"{letter} is in the secret word")
 else:
    print(f"{letter} is not in the secret word")
+
+
+
+
+
+
+
+
+
+grades = {"Sandy" : "A",
+          "Spongebob" : "A+",
+          "Mark" : "B",
+          "Sandy" : "A"}
+
+student = input("Enter the name of the student")
+
+if student in grades:
+    print(f"{student} has a grade of {grades[student]}")
+else:
+    print(f"{student} has not been found")  
