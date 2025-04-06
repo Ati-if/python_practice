79c2b2d36639f2b21f0b9dc770b134827a48e7fd
Ati-if <natif3101@gmail.com>
2025-04-06 12:00:09 +0500

    Iterables

--- iterable.py
+++ iterable.py
@@ -2,3 +2,13 @@ fruits = ("apple" , "orange" , "banana" , "kiwi" , "coconut")
  
 for fruit in fruits:
     print(fruit)
+
+
+
+
+
+name = "atif nawaz"
+
+for character in name:
+    
+    print(character, end=" ")    
