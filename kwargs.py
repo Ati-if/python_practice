666b9329d9c877b827c701eb228ae0e3620697db
Ati-if <natif3101@gmail.com>
2025-04-06 10:47:16 +0500

    kwargs

--- kwargs.py
+++ kwargs.py
@@ -4,3 +4,17 @@ def print_adress(**kwargs):
 
 print_adress(street="123 fake St.",
                      city = "Detroit")
+
+
+
+
+
+
+
+def print_adress(**kwargs):
+    for value in kwargs.values():
+        print(value)
+
+
+print_adress(street="123 fake St.",
+                     city = "Detroit")                    
