fb93b3a9b05c750efd9b61e6e8253196f69520c6
Ati-if <natif3101@gmail.com>
2025-04-07 19:48:00 +0500

    list comprehension practice

--- listcomp.py
+++ listcomp.py
@@ -4,3 +4,19 @@ for x in range(1 , 11):
     doubles.append(x * 2)
 
 print(doubles)  
+
+
+
+
+
+
+
+
+doubles = [x * 2 for x in range(1 , 11)]
+
+print(doubles)
+
+
+triples = [y * 3 for y in range(11 , 25)]
+
+print(triples)
