08187db7f8ea6b01b1d7b3c30b8f04c52dcacbfa
Ati-if <natif3101@gmail.com>
2025-04-09 19:43:06 +0500

    practice of module

--- module.py
+++ module.py
@@ -1,3 +1,20 @@
 import math as m
 
 print(m.pi)
+
+
+
+
+
+
+
+
+from math import pi
+
+a , b , c , d , e = 2 , 5 , 8 , 9 , 6
+
+print(pi ** a)
+print(pi ** b)
+print(pi ** c)
+print(pi ** d)
+print(pi ** e)
