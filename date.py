29a94a947432a08ac320ab379c8894416a224afe
Ati-if <natif3101@gmail.com>
2025-05-05 19:38:25 +0500

    Date in python

---
+++ date.py
@@ -0,0 +1,11 @@
+import datetime
+
+date = datetime.datetime.now()
+print(date)
+print(date.year)
+print(date.month)
+print(date.day)
+print(date.hour)
+print(date.minute)
+print(date.second)
+print(date.microsecond)
