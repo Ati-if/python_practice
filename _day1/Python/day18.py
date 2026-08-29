72ee4375a562eecefe58513985feb544061ac3b3
Ati-if <natif3101@gmail.com>
2026-08-29 07:57:46 +0500

    P7

--- _day1/Python/day18.py
+++ _day1/Python/day18.py
@@ -4,3 +4,14 @@ if word.startswith("A"):
     print("The word starts with A")
 else:
     print("The word does not start with A")
+
+
+
+
+
+word = input("Enter a word: ")
+
+if word.endswith("ing"):
+    print("The word ends with ing")
+else:
+    print("The word does not end with ing")
