e475e00aa56278c597e6382db56e489fb315f2cf
Ati-if <natif3101@gmail.com>
2026-08-29 08:07:47 +0500

    p7

--- _day1/Python/day18.py
+++ _day1/Python/day18.py
@@ -57,3 +57,38 @@ elif choice == 4:
 
 else:
     print("Invalid choice")
+
+
+
+
+
+
+
+
+balance = 5000
+
+print("1. Check Balance")
+print("2. Deposit")
+print("3. Withdraw")
+
+choice = int(input("Enter choice: "))
+
+if choice == 1:
+    print("Balance =", balance)
+
+elif choice == 2:
+    amount = int(input("Enter deposit amount: "))
+    balance = balance + amount
+    print("New balance =", balance)
+
+elif choice == 3:
+    amount = int(input("Enter withdrawal amount: "))
+
+    if amount <= balance:
+        balance = balance - amount
+        print("New balance =", balance)
+    else:
+        print("Insufficient balance")
+
+else:
+    print("Invalid choice")
