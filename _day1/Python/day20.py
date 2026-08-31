113aa7480ecc1be57e0b453831ae2a4d6ab35daf
Ati-if <natif3101@gmail.com>
2026-08-31 12:15:28 +0500

    p9

--- _day1/Python/day20.py
+++ _day1/Python/day20.py
@@ -27,3 +27,21 @@ for number in numbers:
     total += number
 
 print("Total =", total)
+
+
+
+
+
+
+
+
+marks = [70, 80, 65, 90, 75]
+
+total = 0
+
+for mark in marks:
+    total += mark
+
+average = total / len(marks)
+
+print("Average =", average)
