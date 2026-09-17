48e167c9ee0b7a175351e7188b3783636d03b38f
Ati-if <natif3101@gmail.com>
2026-09-17 11:43:48 +0500

    p12

--- _day1/Python/day23.py
+++ _day1/Python/day23.py
@@ -2,3 +2,13 @@ stg1 = "Hello"
 stg2 = "World"
 stg3 = stg1 + " " + stg2
 print(stg3)
+
+
+
+
+
+stg1 = "Hey"
+stg2 = "there"
+stg3 = "All"
+stg = " {} {} , {}! ".format(stg1, stg2, stg3)
+print(stg)
