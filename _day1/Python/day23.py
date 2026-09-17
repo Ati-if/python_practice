dd674b9c4ad433dccbec212bd4f4600f27763a95
Ati-if <natif3101@gmail.com>
2026-09-17 11:51:35 +0500

    p12

--- _day1/Python/day23.py
+++ _day1/Python/day23.py
@@ -30,3 +30,36 @@ for i in range (1 , n+1) :
     for j in range (1 , i+1) :
         print(j, end='')
     print()
+
+
+
+
+
+
+
+r = int(input("Enter the number of rows: "))
+c = int(input("Enter the number of columns: "))
+x = []
+for i_row in range(r):
+    val = []
+    for j_col in range(c):
+        element = int(input(f"Enter the {i_row + 1} * {j_col + 1} element: "))
+        val.append(element)
+    x.append(val)
+
+y = []
+for i_row in range(r):
+    val = []
+    for j_col in range(c):
+        element = int(input(f"Enter the {i_row + 1} * {j_col + 1} element: "))
+        val.append(element)
+    y.append(val)
+
+sum_matrix = []
+for i_row in range(r):
+    val = []
+    for j_col in range(c):
+        val.append(x[i_row][j_col] + y[i_row][j_col])
+    sum_matrix.append(val)
+
+print(sum_matrix)
