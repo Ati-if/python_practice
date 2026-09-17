33ed886b7db73d33465e35a7e3b576f2c171ed78
Ati-if <natif3101@gmail.com>
2026-09-17 11:45:56 +0500

    p12

--- _day1/Python/day23.py
+++ _day1/Python/day23.py
@@ -19,3 +19,14 @@ print(stg)
 d1 = {}
 print(d1)
 print(type(d1))
+
+
+
+
+
+
+n = int(input("Enter a number: "))
+for i in range (1 , n+1) :
+    for j in range (1 , i+1) :
+        print(j, end='')
+    print()
