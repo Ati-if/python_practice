ea40c9644c65b708f7ca402279ae63f23b284eae
Ati-if <natif3101@gmail.com>
2026-09-19 08:36:35 +0500

    p14

---
+++ _day1/Python/day25,2.py
@@ -0,0 +1,16 @@
+from matplotlib import style
+style.use ('ggplot')
+
+x = [2 , 4 , 6]
+y = [12 , 14 , 16]
+
+x2 = [3 , 3 , 5]
+y2 = [7 , 14 , 18]
+import matplotlib.pyplot as plt
+plt.bar (x , y , label = 'Bars 1' , color = 'g')
+plt.bar (x2 , y2 , label = 'Bars 2' , color = 'w')
+plt.xlabel ('Bar Number')
+plt.ylabel ('Bar Height')
+plt.title ('Bar Chart')
+plt.legend()
+plt.show()
