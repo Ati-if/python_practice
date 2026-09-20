8e1d23ee017bd1f9ae9416ed9fa1e942a0fbae5f
Ati-if <natif3101@gmail.com>
2026-09-20 12:43:24 +0500

    p15

--- _day1/Python/day26.py
+++ _day1/Python/day26.py
@@ -29,3 +29,22 @@ x = 30
 y = 50
 
 add(x , y)
+
+
+
+
+
+
+class Person :
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def talk(self) :
+    print("Hi, I am ", self.name)
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+p1.talk()
