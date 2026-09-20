a05dc16fb0fc4d7c76ab493bf453c48966852965
Ati-if <natif3101@gmail.com>
2026-09-20 12:40:53 +0500

    p15

--- _day1/Python/day26.py
+++ _day1/Python/day26.py
@@ -2,3 +2,16 @@ import matplotlib.pyplot as plt
 
 plt.plot([1, 2, 3, 4, 5], [1, 4, 9, 16, 25])
 plt.show()
+
+
+
+
+
+from array import *
+arr = array("i" , [])
+x = int(input("Enter the size of array"))
+print("Enter %d elements" %x)
+for i in range(x):
+  x = int(input("Enter the next value"))
+  arr.append(x)
+  print(arr)
