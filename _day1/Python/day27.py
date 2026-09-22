78217c873ae8b94ebcab3f687e3689da2b494075
Ati-if <natif3101@gmail.com>
2026-09-22 11:47:41 +0500

    p17

--- _day1/Python/day27.py
+++ _day1/Python/day27.py
@@ -29,3 +29,16 @@ pylab.plot(x , y , 'b--')
 pylab.subplot(2 , 2 , 4)
 pylab.plot(y , x , 'y--')
 pylab.show()
+
+
+
+
+
+"""Drawing a subgraph"""
+pylab.subplot(2 , 2 , 1) # The content of the brackets represent(rows , columns , indexes)
+pylab.plot(x , y , 'r--') # The third parameter represent color and line style
+pylab.subplot(2 , 2 , 2)
+pylab.plot(y , x , 'g--')
+pylab.subplot(2 , 2 , 3)
+pylab.plot(x , y , 'b--')
+pylab.show()
