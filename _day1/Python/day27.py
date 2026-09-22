c120c1b1097001773ee29fea7516f120368f24a3
Ati-if <natif3101@gmail.com>
2026-09-22 11:46:15 +0500

    p17

--- _day1/Python/day27.py
+++ _day1/Python/day27.py
@@ -16,3 +16,16 @@ print(np.array([x , y]) . reshape(25 , 2))
 
 pylab.plot(x , y , 'g' , 'w')
 pylab.show()
+
+
+
+"""Drawing a subgraph"""
+pylab.subplot(2 , 2 , 1) # The content of the brackets represent(rows , columns , indexes)
+pylab.plot(x , y , 'r--')
+pylab.subplot(2 , 2 , 2)
+pylab.plot(y , x , 'g--')
+pylab.subplot(2 , 2 , 3)
+pylab.plot(x , y , 'b--')
+pylab.subplot(2 , 2 , 4)
+pylab.plot(y , x , 'y--')
+pylab.show()
