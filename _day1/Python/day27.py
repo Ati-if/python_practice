3a26020423aac72d2dc4c3d9d1a75fb56e058084
Ati-if <natif3101@gmail.com>
2026-09-22 11:39:13 +0500

    p17

--- _day1/Python/day27.py
+++ _day1/Python/day27.py
@@ -1,29 +1,3 @@
 from matplotlib import pylab
 print(pylab.__version__)
-"""Use NumPy to generate random data"""
-import numpy as np
-x = np.linspace(0 , 10 , 25)
-y = x * x + 2
-print(x)
-print(y)
 
-
-
-print(np.array([x , y]) . reshape(25 , 2))
-
-
-"""It only takes only one command to draw"""
-pylab.plot(x , y , 'g' , 'w') # r stands for red
-
-
-
-
-"""Drawing a subgraph"""
-pylab.subplot(2 , 2 , 1) # The content of the brackets represent(rows , columns , indexes)
-pylab.plot(x , y , 'r--')
-pylab.subplot(2 , 2 , 2)
-pylab.plot(y , x , 'g--')
-pylab.subplot(2 , 2 , 3)
-pylab.plot(x , y , 'b--')
-pylab.subplot(2 , 2 , 4)
-pylab.plot(y , x , 'y--')
