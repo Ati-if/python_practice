330c986382c503acda6b27ca7342509f6f2c0969
Ati-if <natif3101@gmail.com>
2026-09-23 08:31:54 +0500

    p18

--- _day1/Python/day28.py
+++ _day1/Python/day28.py
@@ -1,33 +1,36 @@
+# Example 1: vertical subplots
 from matplotlib import pyplot as plt
-
-"""Drawing a subgraph"""
 
 x = [1, 2, 3, 4, 5]
 y = [5, 4, 3, 2, 1]
 
-plt.subplot(2, 1, 1)  # rows, columns, index
-plt.plot(x, y, 'r--')  # third parameter = color and line style
+plt.subplot(2, 1, 1)
+plt.plot(x, y, 'r--')
 
 plt.subplot(2, 1, 2)
 plt.plot(y, x, 'g+-')
 
+plt.show()
 
-
+# Example 2: horizontal subplots
 import pylab
-pylab.subplot(1 , 2 , 1)
-pylab.plot(x , y , 'r--')
-pylab.subplot(1 , 2 , 2)
-pylab.plot(y , x , 'g+-')
-pylab.subplot(1 , 2 , 1)
-pylab.plot(y , x , 'g+-')
+
+pylab.subplot(1, 2, 1)
+pylab.plot(x, y, 'r--')
+
+pylab.subplot(1, 2, 2)
+pylab.plot(y, x, 'g+-')
+
 pylab.show()
 
-
+# Example 3: custom axes
 from matplotlib import pyplot as plt
-fig = pylab.figure()
-axis = fig.add_axes([0.1, 0.1, 0.8, 0.8])    #control left , right , width , height of canvas(from 0 to 1)
-axis.plot(x , y , 'b')
+
+fig = plt.figure()
+axis = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+axis.plot(x, y, 'b')
 axis.set_xlabel('X-Label')
 axis.set_ylabel('Y-Label')
 axis.set_title('Title')
+
 plt.show()
