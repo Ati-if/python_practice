c1e11d535548375c1f947a1e3a285ceb379f5a3a
Ati-if <natif3101@gmail.com>
2026-09-23 08:18:20 +0500

    p18

--- _day1/Python/day28.py
+++ _day1/Python/day28.py
@@ -11,3 +11,13 @@ plt.plot(x, y, 'r--')  # third parameter = color and line style
 plt.subplot(2, 1, 2)
 plt.plot(y, x, 'g+-')
 plt.show()
+
+
+import pylab
+pylab.subplot(1 , 2 , 1)
+pylab.plot(x , y , 'r--')
+pylab.subplot(1 , 2 , 2)
+pylab.plot(y , x , 'g+-')
+pylab.subplot(1 , 2 , 1)
+pylab.plot(y , x , 'g+-')
+pylab.show()
