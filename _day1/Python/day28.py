e632e8e1161f135ec5443f173be0c519cbbac5e7
Ati-if <natif3101@gmail.com>
2026-09-23 08:56:35 +0500

    p18

--- _day1/Python/day28.py
+++ _day1/Python/day28.py
@@ -45,3 +45,17 @@ for ax in axes.flat:
 
 plt.tight_layout()
 plt.show()
+
+
+
+
+
+
+
+#Draw a picture or graph inside another graph
+fig = pylab.figure()
+axis1 = fig.add_axes([0.1 , 0.1 , 0.8 , 0.8])  #big axis
+axis2 = fig.add_axes([0.2 , 0.5 , 0.3 , 0.4])  #small canvas
+axis1.plot(x , y , 'b')
+axis2.plot(y , x , 'r')
+pylab.show()
