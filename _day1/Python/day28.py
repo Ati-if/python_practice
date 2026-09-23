bbbabcd0b72dea96039a26be55b7a656ed233b81
Ati-if <natif3101@gmail.com>
2026-09-23 08:27:38 +0500

    p18

--- _day1/Python/day28.py
+++ _day1/Python/day28.py
@@ -10,7 +10,7 @@ plt.plot(x, y, 'r--')  # third parameter = color and line style
 
 plt.subplot(2, 1, 2)
 plt.plot(y, x, 'g+-')
-plt.show()
+
 
 
 import pylab
@@ -21,3 +21,13 @@ pylab.plot(y , x , 'g+-')
 pylab.subplot(1 , 2 , 1)
 pylab.plot(y , x , 'g+-')
 pylab.show()
+
+
+from matplotlib import pyplot as plt
+fig = pylab.figure()
+axis = fig.add_axes([0.1, 0.1, 0.8, 0.8])    #control left , right , width , height of canvas(from 0 to 1)
+axis.plot(x , y , 'b')
+axis.set_xlabel('X-Label')
+axis.set_ylabel('Y-Label')
+axis.set_title('Title')
+plt.show()
