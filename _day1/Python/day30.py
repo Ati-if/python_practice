ccc9e358d23db4364f0a576b82687341278d0c4d
Ati-if <natif3101@gmail.com>
2026-09-25 08:53:50 +0500

    p20

--- _day1/Python/day30.py
+++ _day1/Python/day30.py
@@ -12,3 +12,13 @@ print(s1)
 order = [1 , 2 , 3 , 4 , 5 ]
 s2 = pd.Series(arr , index = order)
 print(s2)
+
+
+
+
+
+import numpy as np
+n = np.random.randn(5)
+index = ['a' , 'b' , 'c' , 'd' , 'e']
+s3 = pd.Series(n , index = index)
+print(s3)
