b315dd5e942933bac51464758fbb6ce28489936e
Ati-if <natif3101@gmail.com>
2026-09-26 08:53:13 +0500

    p21

--- _day1/Python/day31.py
+++ _day1/Python/day31.py
@@ -46,3 +46,22 @@ time = float(input("Enter the time in years :"))
 interest = (principal * rate * time) / 100
 
 print("Simple Interest = " , interest)
+
+
+
+
+
+print("1 : Say Hello")
+print("2 : Say Goodbye")
+print("3 : Exit")
+
+choice = int(input("Enter your choice :"))
+
+if choice == 1:
+    print("Hello!")
+elif choice == 2:
+    print("Goodbye!")
+elif choice == 3:
+    print("Exiting...")
+else:
+    print("Invalid choice!")
