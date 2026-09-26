f4c72e89864f2559c0193bd3f763e5efc4f12d54
Ati-if <natif3101@gmail.com>
2026-09-26 08:43:12 +0500

    p21

--- _day1/Python/day31.py
+++ _day1/Python/day31.py
@@ -24,3 +24,13 @@ celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 9 / 5) + 32
 
 print("Fahrenheit =", fahrenheit)
+
+
+
+
+
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print("Celsius =", celsius)
